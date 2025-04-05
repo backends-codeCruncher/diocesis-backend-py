e9c387ea04f466e5f0953650e40b6e9be9518549
wing771201 <garciacruz771@gmail.com>
2025-04-04 18:08:28 -0700

    update: agregar campos a padres

--- padres/models.py
+++ padres/models.py
@@ -10,6 +10,10 @@ class Padre(BaseModel):
     lastName = models.CharField(max_length=100)
     birthDate = models.DateField()
     picture = CloudinaryField('image', blank=True, null=True)
+    email = models.EmailField(null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.firstName} {self.lastName}"
